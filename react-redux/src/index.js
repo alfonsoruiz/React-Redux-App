fd6833df2c090e6cd04fb6375a74fc14765b3d38
alfonsoruiz <1800ruiz@gmail.com>
2019-09-28 16:11:12 -0700

    created react app, added necessary dependencies

---
+++ react-redux/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux';
+
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
